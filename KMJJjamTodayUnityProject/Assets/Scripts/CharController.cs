38243eb5c0d7816e645d70294619b89d8b24b624
KAGSme <kags1995@hotmail.co.uk>
2015-09-11 22:14:33 +0100

    more init

---
+++ KMJJjamTodayUnityProject/Assets/Scripts/CharController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+    public float maxSpeed = 100;
+    Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddRelativeForce(new Vector2(Input.GetAxis("Horizontal") * maxSpeed, Input.GetAxis("Vertical") * maxSpeed));
+	}
+}
