b798b3e77f3c3bb8907dcd0a6178ea8579db014f
KAGSme <kags1995@hotmail.co.uk>
2015-09-12 18:33:41 +0100

    enemy graphics and ammo

---
+++ KMJJjamTodayUnityProject/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour {
+
+    public static PlayerData pd;
+    public int maxHealth;
+    int health;
+    int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
