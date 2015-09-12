a8ad14cf6207b245aed8ef22b8c5de6c2d1b07ab
jimc1664 <jimc1664@outlook.com>
2015-09-12 15:11:31 +0100

    mouse alternative aim - we aren't all peasants

--- KMJJjamTodayUnityProject/Assets/Scripts/CharAiming.cs
+++ KMJJjamTodayUnityProject/Assets/Scripts/CharAiming.cs
@@ -9,21 +9,40 @@ public class CharAiming : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var angle = Mathf.Atan2(-Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical")) * Mathf.Rad2Deg;
-        if (Input.GetAxis("RVertical") == 0 && Input.GetAxis("RHorizontal") == 0) rotObject.gameObject.SetActive(false);
-        else rotObject.gameObject.SetActive(true);
-            rotObject.transform.rotation = Quaternion.Euler(0,0, angle);
+        if(Input.GetJoystickNames().Length > 0) { 
+            var angle = Mathf.Atan2(-Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical")) * Mathf.Rad2Deg;
+            if (Input.GetAxis("RVertical") == 0 && Input.GetAxis("RHorizontal") == 0) rotObject.gameObject.SetActive(false);
+            else{
+                rotObject.gameObject.SetActive(true);
+                rotObject.transform.rotation = Quaternion.Euler(0,0, angle);
 
-            if (Input.GetAxis("FireR") > 0 /*&& isLoaded*/)
-            {
-                var laser = (GameObject)Instantiate(laserPrefab, transform.position, Quaternion.Euler(0,0,angle));
-                laser.GetComponent<Laser>().angle = new Vector2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"));
-                isLoaded = false;
+                if (Input.GetAxis("FireR") > 0 /*&& isLoaded*/)
+                {
+                    var laser = (GameObject)Instantiate(laserPrefab, transform.position, Quaternion.Euler(0,0,angle));
+                    laser.GetComponent<Laser>().angle = new Vector2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"));
+                    isLoaded = false;
+                }
             }
+        } else { 
+            var vec = (Vector2) Camera.main.ScreenToWorldPoint( Input.mousePosition ) - (Vector2)transform.position;
+            if( vec.sqrMagnitude < 0.01f ) rotObject.gameObject.SetActive(false);
+            else{ 
+                rotObject.gameObject.SetActive(true);
+                rotObject.transform.rotation = Quaternion.Euler(0,0, Mathf.Atan2(-vec.x, vec.y) * Mathf.Rad2Deg);
+
+                if (Input.GetAxis("Fire1") > 0 /*&& isLoaded*/)
+                {
+                    var laser = (GameObject)Instantiate(laserPrefab, transform.position, rotObject.transform.rotation );
+                    laser.GetComponent<Laser>().angle = vec;
+                    isLoaded = false;
+                }
+            }
+        }
 	}
+
 }
