5fa1b8af679bed8ec395756e648cb20f3e79ecc6
KAGSme <kags1995@hotmail.co.uk>
2015-09-12 11:10:12 +0100

    blackhole moving

--- KMJJjamTodayUnityProject/Assets/Scripts/Blackhole.cs
+++ KMJJjamTodayUnityProject/Assets/Scripts/Blackhole.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Blackhole : MonoBehaviour {
 
-    public float maxForce;
+    public float maxForce= 500;
+    public float speed = 20;
     Rigidbody2D player;
+    public Transform[] travelPoints;
+    Transform currentDestination;
+    public bool moves = false;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
 	}
+
+    void Update()
+    {
+        if (moves)
+        {
+            if (currentDestination == null)
+            {
+                currentDestination = travelPoints[Random.Range(0, travelPoints.Length)];
+            }
+            if (currentDestination != null)
+            {
+                transform.position = Vector3.Slerp(transform.position, currentDestination.position, Time.deltaTime * speed);
+                if (Mathf.Abs(Vector3.Magnitude(currentDestination.position - transform.position)) < 4.0f)
+                {
+                    currentDestination = travelPoints[Random.Range(0, travelPoints.Length)];
+                }
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
