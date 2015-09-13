bd7872499f72de278661f6b1fc0e8622dc8e2f0c
Mark Tempini <marktempini@googlemail.com>
2015-09-13 02:05:28 +0100

    Menu Linked

---
+++ KMJJjamTodayUnityProject/Assets/Scripts/ButtonNextLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNextLevel : MonoBehaviour
+{
+
+    public void NextLevelButton(string levelName)
+    {
+        Application.LoadLevel(levelName);
+    }
+}
