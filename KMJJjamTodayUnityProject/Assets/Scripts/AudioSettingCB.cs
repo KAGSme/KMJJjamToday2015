df6fa3eb0e63ac24fb09e86c821d51e147c5c230
jimc1664 <jimc1664@outlook.com>
2015-09-13 14:37:24 +0100

    adas

---
+++ KMJJjamTodayUnityProject/Assets/Scripts/AudioSettingCB.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSettingCB : MonoBehaviour {
+
+    public void CB(float valueChange)
+    {
+         AudioListener.volume = valueChange; 
+    }
+
+}
